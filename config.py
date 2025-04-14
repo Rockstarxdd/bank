660cb153ccfc3180a7deb40670c47af792fa3918
ROCKSTAR OP <88956892+Rockstarxdd@users.noreply.github.com>
2025-04-14 18:09:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 # config.py
-API_ID = 10457965  # my.telegram.org se lo
+API_ID = 10457965  # my.telegram.org se lein
 API_HASH = "7f0667b9acdc8bbb498a2f2f1cc5bbdf"
-BOT_TOKEN = "your_bot_token"
 
-GROUP_CHAT_ID = -1002023711351  # jis group se scrap karna hai uska Chat ID
+GROUP_CHAT_ID = -1002023711351  # us private group ka chat ID
