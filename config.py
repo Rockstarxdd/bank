df55ca1b545857d70b7144db67e060b226a8d5a7
ROCKSTAR OP <88956892+Rockstarxdd@users.noreply.github.com>
2025-04-14 18:02:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ API_ID = 12345678  # my.telegram.org se lo
 API_HASH = "your_api_hash"
 BOT_TOKEN = "your_bot_token"
 
-GROUP_CHAT_ID = -1001234567890  # jis group se scrap karna hai uska Chat ID
+GROUP_CHAT_ID = -1002023711351  # jis group se scrap karna hai uska Chat ID
