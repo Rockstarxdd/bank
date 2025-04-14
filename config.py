26e33ae71ed1d89dcb8d9fb5b95df0c99ac86ab1
ROCKSTAR OP <88956892+Rockstarxdd@users.noreply.github.com>
2025-04-14 18:01:20 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+API_ID = 12345678  # my.telegram.org se lo
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+
+GROUP_CHAT_ID = -1001234567890  # jis group se scrap karna hai uska Chat ID
